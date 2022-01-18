5b78328c863b4de06d7a8782661ffbd71b168b0e
Eze Vazquez <yourgitemail@example.com>
2022-01-18 11:59:49 -0300

    ultimas modificaciones

--- codePG/settings.py
+++ codePG/settings.py
@@ -38,9 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'registro',
-    'login',
-    'mensajeria',
-    'perfiles',
 ]
 
 MIDDLEWARE = [
--- registro/models.py
+++ registro/models.py
@@ -6,3 +6,4 @@ class Usuarios(models.Model):
     usuario = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     nombre = models.CharField(max_length=50)
+
