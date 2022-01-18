bcaf21b13e0a8dd029134ddd84968941895ad704
Eze Vazquez <yourgitemail@example.com>
2022-01-17 21:39:04 -0300

    Modificación de template registro

--- registro/models.py
+++ registro/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+class Usuarios(models.Model):
+    usuario = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    nombre = models.CharField(max_length=50)
